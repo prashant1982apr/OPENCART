ba1bbcc20b052ea2545c8890c7174a6bb896273e
prashant1982apr <mrprash2003@gmail.com>
2022-08-20 14:48:46 +0530

    SECOND COMMIT

---
+++ src/test/java/com/qa/opencart/tests/test123.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class test123 {
+	
+	@Test
+	public void add() {
+		System.out.println("test123 successful");
+	}
+
+}
